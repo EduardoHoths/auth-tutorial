8d65ec2604608b2b8ee05458d5bdfd3634027f94
Eduardo <eduardo.hoths@gmail.com>
2024-10-23 23:03:48 -0300

    feat: home page created

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AuthLayout = () => {
+  return (
+    <div>
+      <nav className='bg-red text-white'>
+        This is auth navbar
+      </nav>
+    </div>
+  )
+}
+
+export default AuthLayout
