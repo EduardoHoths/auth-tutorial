27ab9601e29ea55eaf59759be2180dc12f474330
Eduardo Hoths <desenvolvimento@nulogistica.com.br>
2024-10-25 12:36:13 -0300

    feat: register form created

--- schemas/index.ts
+++ schemas/index.ts
@@ -8,7 +8,11 @@ export const LoginSchema = z.object({
 });
 
 export const RegisterSchema = z.object({
-  email: z.string().email(),
-  password: z.string(),
-  name: z.string(),
+  email: z.string().email({
+    message: "Email is required",
+  }),
+  password: z.string().min(6, { message: "Minimum 6 characters required." }),
+  name: z.string().min(1, {
+    message: "Name is required",
+  }),
 });
