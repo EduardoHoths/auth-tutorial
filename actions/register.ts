5d77bb246e997e74f3a955d21ba75eccad5709fd
Eduardo <eduardo.hoths@gmail.com>
2024-10-26 21:23:48 -0300

    feat: create user function created

--- actions/register.ts
+++ actions/register.ts
@@ -2,6 +2,9 @@
 
 import { RegisterSchema } from "@/schemas";
 import { z } from "zod";
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -9,6 +12,26 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   if (!validatedFields.success) {
     return { error: "Invalid fields!" };
   }
+
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email)
+
+  if (existingUser) {
+    // This is a generic error message, because we don't want to reveal the existence of the email.
+    return { success: "Email sent!" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+  
+  // TODO: Send verification token email
 
   return { success: "Email sent!" };
 };
