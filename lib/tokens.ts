dc9755c70b8323cd6fadca4663b17a3629c9caff
Eduardo Hoths <desenvolvimento@nulogistica.com.br>
2024-11-04 10:35:33 -0300

    feat: user button created

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -2,13 +2,13 @@ import { v4 as uuidv4 } from "uuid";
 import { db } from "./db";
 import { getVerificationTokenByEmail } from "@/data/verification-token";
 import { getPasswordResetTokenByEmail } from "@/data/password-reset-token";
-import crypto from "crypto"
+import crypto from "crypto";
 import { getTwoFactorTokenByEmail } from "@/data/two-factor-token";
 
 export const generateTwoFactorToken = async (email: string) => {
   const token = crypto.randomInt(100_000, 1_000_000).toString();
-  // TODO: Later change to 15 minutes
-  const expires = new Date(new Date().getTime() + 3600 * 1000);
+  
+  const expires = new Date(new Date().getTime() + 5 * 60 * 1000);
 
   const existingToken = await getTwoFactorTokenByEmail(email);
 
@@ -28,8 +28,8 @@ export const generateTwoFactorToken = async (email: string) => {
     },
   });
 
-  return twoFactorToken
-}
+  return twoFactorToken;
+};
 
 export const generateVerificationToken = async (email: string) => {
   const token = uuidv4();
